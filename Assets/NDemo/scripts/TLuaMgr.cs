7d3ced30bcd2ac1afe98e2de71accf108fd499c4
rab000 <ce1985@126.com>
2019-06-16 14:26:42 +0800

    更新文档

---
+++ Assets/NDemo/scripts/TLuaMgr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace NXLua {
+
+    public class TLuaMgr : MonoBehaviour
+    {
+
+        public static LuaEnv _LuaEnv = new LuaEnv();
+        
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+
