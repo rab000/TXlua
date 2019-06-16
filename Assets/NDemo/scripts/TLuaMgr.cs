af281db65f58ab02b03a9b36b14fe04f35d02904
rab000 <ce1985@126.com>
2019-06-16 15:04:21 +0800

    ff

--- Assets/NDemo/scripts/TLuaMgr.cs
+++ Assets/NDemo/scripts/TLuaMgr.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using XLua;
+using System.IO;
 
 namespace NXLua {
 
@@ -9,15 +10,25 @@ namespace NXLua {
     {
 
         public static LuaEnv _LuaEnv = new LuaEnv();
-        
-        void Start()
-        {
 
+        public static TLuaMgr Ins;
+
+        private void Awake()
+        {
+            Ins = this;
+
+            _LuaEnv.AddLoader(MyLoader);
         }
 
-        void Update()
+        void Start()
         {
+            _LuaEnv.DoString(string.Format("package.path = '{0}/?.lua'", Application.dataPath));
+        }
 
+        private byte[] MyLoader(ref string filePath)
+        {
+            string absPath = @"E:appunity2018productsFishXlua" + filePath + ".lua.txt";
+            return System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(absPath));
         }
     }
 }
