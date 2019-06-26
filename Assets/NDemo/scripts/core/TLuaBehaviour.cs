d2308614cd80fbae56f6e2e256ba73668bc628df
cuizheng <cuizheng@fotoable.com>
2019-06-26 20:34:42 +0800

    窗口热更lua基本框架完成

--- Assets/NDemo/scripts/core/TLuaBehaviour.cs
+++ Assets/NDemo/scripts/core/TLuaBehaviour.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Tencent is pleased to support the open source community by making xLua available.
- * Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
+﻿
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -24,6 +17,7 @@ namespace NXLua
     [LuaCallCSharp]
     public class TLuaBehaviour : MonoBehaviour
     {
+        public static Dictionary<string, LuaFunction> LuaDic = new Dictionary<string, LuaFunction>();
         //public TextAsset luaScript;
         //lua文件沙盒中相对位置,自定义loader再TLuaMgr中就已经设置好了，这类直接传入相对路径
         public string LuaRelePath;
@@ -40,10 +34,10 @@ namespace NXLua
 
         private LuaTable scriptEnv;
 
-        string s = 
-            @"function start()" +
-            "print('injected object', btn1)" +
-            "end";
+        //string s = 
+        //    @"function start()" +
+        //    "print('injected object', btn1)" +
+        //    "end";
 
         public void Awake()
         {
@@ -56,7 +50,7 @@ namespace NXLua
                 return;
             }
 
-            BeLoadLuaStr = true;
+            
             Debug.Log("TLuaBehaviour.Awake LuaRelePath!=null 继续执行Awake");
 
             scriptEnv = TLuaMgr._LuaEnv.NewTable();
@@ -79,19 +73,55 @@ namespace NXLua
             }
             Debug.Log("TLuaBehaviour.Awake 设置self 各种go环境完毕");
 
-            //NTODO 下一步 ，require加载的时候，后面的scriptEnv没生效，要研究下怎么给一段lua设置环境
+            //nafio inforequire加载的时候，后面的scriptEnv没生效，
+            //但是直接加载str是生效的，要研究下怎么给一段lua设置环境
             //_LuaEnv.DoString(string.Format("require '{0}'",LuaRelePath)/*luaScript.text*/, "TLuaBehaviour", scriptEnv);
-            TLuaMgr._LuaEnv.DoString(s/*luaScript.text*/, "TLuaBehaviour", scriptEnv);
+            //TLuaMgr._LuaEnv.DoString(s/*luaScript.text*/, "TLuaBehaviour", scriptEnv);
+
+            //这样可以缓存lua代码段
+            LuaFunction luaFunc = null;
+            if (LuaDic.ContainsKey(LuaRelePath))
+            {
+                luaFunc = LuaDic[LuaRelePath];
+                BeLoadLuaStr = true;
+                AfterLuaLoaded(luaFunc);
+            }
+            else
+            {
+                //NTODO 这里如果从bundle加载，要做异步回调处理
+                Debug.LogError("加载路径--->"+ LuaRelePath);
+                TextAsset textAsset = Resources.Load<TextAsset>(LuaRelePath+".lua");
+                Debug.LogError("加载到的txt--->" + textAsset.text);
+                //先加载lua txt 然后再导入后续string,这里加载需要时间，是个异步操作
+                luaFunc = TLuaMgr._LuaEnv.LoadString(textAsset.text, "TLuaBehaviour", scriptEnv);
+                LuaDic.Add(LuaRelePath, luaFunc);
+                AfterLuaLoaded(luaFunc);
+
+            }
+
+        }
+
+        private void AfterLuaLoaded(LuaFunction luaFunc)
+        {
+            BeLoadLuaStr = true;
+
+            luaFunc.SetEnv(scriptEnv);
+
+            luaFunc.Call();
 
             Action luaAwake = scriptEnv.Get<Action>("awake");
+
             scriptEnv.Get("start", out luaStart);
+
             scriptEnv.Get("update", out luaUpdate);
+
             scriptEnv.Get("ondestroy", out luaOnDestroy);
 
             if (luaAwake != null)
             {
                 luaAwake();
             }
+
         }
 
         // Use this for initialization
--- Assets/NDemo/scripts/core/TLuaHotFixMgr.cs
+++ Assets/NDemo/scripts/core/TLuaHotFixMgr.cs
@@ -10,7 +10,7 @@ public class TLuaHotFixMgr : MonoBehaviour {
 
 	void Awake(){
 		TLuaMgr._LuaEnv.AddLoader (MyLoader);   
-		//NTODO 这里貌似需要载入所有需要热更的lua,
+		//NTODO 这里貌似需要载入所有需要热更的lua,貌似这个类都不需要了，main.lua接管了
 
 		TLuaMgr._LuaEnv.DoString ("require 'hot'");
 	}
