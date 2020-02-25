93159bb03265b171ebb50b66a916f472390327c4
rab000 <ce1985@126.com>
2020-02-25 22:25:46 +0800

    加注释

--- Assets/NLuaFramework/scripts/core/TLuaBehaviour.cs
+++ Assets/NLuaFramework/scripts/core/TLuaBehaviour.cs
@@ -1,6 +1,5 @@
 ﻿
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using XLua;
 using System;
@@ -13,6 +12,14 @@ namespace NXLua
         public string name;
         public GameObject value;
     }
+
+    /// <summary>
+    /// NINFO 这个类就是解决lua不能直接挂载成mono的问题
+    /// 具体方法为
+    /// 1 把这个mono类挂载到go
+    /// 2 通过一个LuaTable scriptEnvTable，收集有关这个mono类的信息（mono实例，子节点go等）
+    /// 3 Awake中通过一个string类型的path加载lua代码块时把这个scriptEnvTable传递过去
+    /// </summary>
 
     [LuaCallCSharp]
     public class TLuaBehaviour : MonoBehaviour
@@ -34,12 +41,9 @@ namespace NXLua
 
         private Action luaOnDestroy;
 
+        //这个lua表里会存储MonoBehaviour传给lua的一些参数
+        //然后加载lua代码块时，会把这个表中传过去
         private LuaTable scriptEnvTable;
-
-        //string s = 
-        //    @"function start()" +
-        //    "print('injected object', btn1)" +
-        //    "end";
 
         public void Awake()
         {
@@ -56,15 +60,20 @@ namespace NXLua
             Debug.Log("TLuaBehaviour.Awake LuaRelePath!=null 继续执行Awake");
 
             scriptEnvTable = TLuaMgr._LuaEnv.NewTable();
-           
+
             // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+            // 具体看AfterLuaLoaded中对luaFunc.SetEnv(scriptEnvTable);的解释
             LuaTable meta = TLuaMgr._LuaEnv.NewTable();
             meta.Set("__index", TLuaMgr._LuaEnv.Global);
-            
+
+            //元表用来处理表之间的处理关系，比如这里meta表实现了__index,
+            //当scriptEnvTable使用scriptEnvTable[xxx]找不到时，就会通过__index到TLuaMgr._LuaEnv.Global中查找
             scriptEnvTable.SetMetaTable(meta);
             meta.Dispose();
 
-            scriptEnvTable.Set("self", this);//因为这句，暂时不能将所有lua统一从main.lua入口调用，在每个luaMono中单独调用
+            //因为这句，暂时不能将所有lua统一从main.lua入口调用，在每个luaMono中单独调用
+            //这句的意思时表scriptEnvTable就是TluaBehaviour，lua self相当与csharp中的this
+            scriptEnvTable.Set("self", this);
 
             if (null != injections)
             {
@@ -95,6 +104,8 @@ namespace NXLua
                 TextAsset textAsset = Resources.Load<TextAsset>(LuaRelePath+".lua");
                 Debug.LogError("加载到的txt--->" + textAsset.text);
                 //先加载lua txt 然后再导入后续string,这里加载需要时间，是个异步操作
+                //加载一个代码块，但不执行，只返回类型可以指定为一个delegate或者一个LuaFunction
+                //第3个参数时这个代码块的环境变量
                 luaFunc = TLuaMgr._LuaEnv.LoadString(textAsset.text, "TLuaBehaviour", scriptEnvTable);
                 LuaDic.Add(LuaRelePath, luaFunc);
                 AfterLuaLoaded(luaFunc);
@@ -107,12 +118,24 @@ namespace NXLua
         {
             BeLoadLuaStr = true;
 
+            //?这里和awake中重复了貌似
+            //这里xlua的解释相当于lua的setfenv函数
+            //setfenv的解释https://www.cnblogs.com/sifenkesi/p/3843348.html
+            //猜测这句的意思就是为了避免luaFunc这段代码里面一些变量跟lua全局变量同名
+            //给luaFunc这段代码设置了一个（非全局）环境(就是一个表)，
+            //再直白点就是luaFunc只能访问这个非全局环境（表）中的域，或者自己函数本身的域
+            //这里的意思就是lua代码段只能访问自己或者TluaBehaviour这个类
             luaFunc.SetEnv(scriptEnvTable);
 
+            //执行lua代码段
             luaFunc.Call();
 
+            //从lua代码段中取出名为awake的函数，？为什么能取到呢，表应该只包含TLuaBehaviour和lua.Globle,
+            //怎么可以取到lua代码段中的代码呢，难道是从全局里取到的？
             Action luaAwake = scriptEnvTable.Get<Action>("awake");
 
+            //从lua代码段中取出名为start的函数，并复制给luaStart这个委托，这样，通过这个委托，在Start中
+            //就可以调用lua代码中的start了
             scriptEnvTable.Get("start", out luaStart);
 
             scriptEnvTable.Get("update", out luaUpdate);
